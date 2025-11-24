2a96a14177b82319cee9a1fc4fe77381519ddf36
Raman  Sah <145567353+MisterStranger03@users.noreply.github.com>
2025-11-24 19:48:29 +0530

    Update Issue.js

--- backend/models/Issue.js
+++ backend/models/Issue.js
@@ -1,21 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const issueSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   priority: { type: String, required: true },
-//   priorityLevel: { type: String, required: true },
-//   description: { type: String, required: true },
-//   address: { type: String, required: true },
-//   images: { type: [String], required: true },
-//   username: { type: String, required: true },
-//   latitude: { type: Number },
-//   longitude: { type: Number },
-//   status: { type: String, default: 'Open' },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.model('Issue', issueSchema);
-
 const mongoose = require('mongoose');
 const Log = require('../models/Log');
 
